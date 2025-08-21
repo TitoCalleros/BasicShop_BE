c15cf1115ba2d4a68c8cf50e68e10802d414dcc0
Tito Calleros <tito.calleros@gmail.com>
2025-08-21 13:07:23 -0600

    Update Description property to nullable string

--- BasicShopAPI/Domain/Entities/Product.cs
+++ BasicShopAPI/Domain/Entities/Product.cs
@@ -7,13 +7,13 @@ namespace BasicShopAPI.Domain.Entities
 
         public Guid Id { get; private set; }
         public string Name { get; private set; }
-        public string Description { get; private set; }
+        public string? Description { get; private set; }
         public decimal Price { get; private set; }
         public int Stock { get; private set; }        
 
-        public Product(string name, string description, decimal price, int stock) => Update(name, description, price, stock);
+        public Product(string name, string? description, decimal price, int stock) => Update(name, description, price, stock);
 
-        public void Update(string name, string description, decimal price, int stock)
+        public void Update(string name, string? description, decimal price, int stock)
         {
             if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentException("Name cannot be null");

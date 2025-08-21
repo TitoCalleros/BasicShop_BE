64a1187aa9b432f36e5a8f48a57f27c338d22727
Tito Calleros <tito.calleros@gmail.com>
2025-08-21 12:24:36 -0600

    Change Id property type to Guid

--- BasicShopAPI/Domain/Entities/Product.cs
+++ BasicShopAPI/Domain/Entities/Product.cs
@@ -5,11 +5,11 @@ namespace BasicShopAPI.Domain.Entities
     public class Product
     {
 
-        public int Id { get; private set; }
+        public Guid Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
         public decimal Price { get; private set; }
-        public int Stock { get; private set; }
+        public int Stock { get; private set; }        
 
         public Product(string name, string description, decimal price, int stock) => Update(name, description, price, stock);
 

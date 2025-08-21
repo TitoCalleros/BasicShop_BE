b6bcd8a9fc175b41a76bb8bdf142b41f47dab9a5
Tito Calleros <tito.calleros@gmail.com>
2025-08-21 00:36:02 -0600

    Added the migration to create the Products table.

---
+++ BasicShopAPI/Infrastructure/Migrations/Migration_20250820233010_CreateProduct.cs
@@ -0,0 +1,36 @@
+﻿using FluentMigrator;
+
+namespace BasicShopAPI.Infrastructure.Migrations
+{
+    [Migration(20250820233010)]
+    public class Migration_20250820233010_CreateProduct : Migration
+    {
+        private string _tableName = "Products";
+
+        public override void Up()
+        {
+            Create.Table(_tableName)
+                .WithColumn("Id").AsGuid().PrimaryKey().Identity()
+                .WithColumn("Name").AsString(150).NotNullable()
+                .WithColumn("Description").AsString(500).Nullable()
+                .WithColumn("Price").AsDecimal(10, 2).NotNullable()
+                .WithColumn("Stock").AsInt32().NotNullable().WithDefaultValue(0)
+                .WithColumn("CreatedAt").AsDateTime().NotNullable()
+                    .WithDefault(SystemMethods.CurrentDateTime);
+
+            Execute.Sql($"ALTER TABLE {_tableName} ADD CONSTRAINT CK_{_tableName}_Price_Pos CHECK (Price > 0)");
+            Execute.Sql($"ALTER TABLE {_tableName} ADD CONSTRAINT CK_{_tableName}_Stock_NonNegative CHECK (Stock >= 0)");
+
+            Create.Index($"IX_{_tableName}_Name")
+                .OnTable(_tableName).OnColumn("Name").Ascending();           
+        }
+
+        public override void Down()
+        {
+            Execute.Sql($"ALTER TABLE {_tableName} DROP CONSTRAINT CK_{_tableName}_Price_Pos");
+            Execute.Sql($"ALTER TABLE {_tableName} DROP CONSTRAINT CK_{_tableName}_Stock_NonNegative");
+
+            Delete.Table(_tableName);
+        }
+    }
+}

e45982f54128cf6cbd5dee3c47842108ea1b472c
Tito Calleros <tito.calleros@gmail.com>
2025-08-20 21:45:19 -0600

    FluentMigrator configuration

--- BasicShopAPI/Program.cs
+++ BasicShopAPI/Program.cs
@@ -1,12 +1,30 @@
+using FluentMigrator.Runner;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 // Add services to the container.
+
+builder.Services.AddFluentMigratorCore()
+    .ConfigureRunner(rn => rn.AddSqlServer2012()
+        .WithGlobalConnectionString(connectionString)
+        .ScanIn(typeof(Program).Assembly).For.Migrations())
+    .AddLogging(log => log.AddFluentMigratorConsole());
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+    runner.MigrateUp();
+}
+
+app.MapGet("/", () => "Migrations applied");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

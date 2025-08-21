053d65440638e549f15db3c6a791df087fcf904c
Tito Calleros <tito.calleros@gmail.com>
2025-08-21 00:37:33 -0600

    Create the Product entity and Infraestructure folders

---
+++ BasicShopAPI/Domain/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BasicShopAPI.Domain.Entities
+{
+    public class Product
+    {
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+
+        public Product(string name, string description, decimal price, int stock) => Update(name, description, price, stock);
+
+        public void Update(string name, string description, decimal price, int stock)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name cannot be null");
+
+            if (price <= 0)
+                throw new ArgumentException("Price must be greater than 0");
+
+            if (stock < 0)
+                throw new ArgumentException("stock cannot be negative");
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+        }
+    }
+}

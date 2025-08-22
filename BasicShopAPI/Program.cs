91f904691da4adb730f499fee5fd78c1aebe56bb
Tito Calleros <tito.calleros@gmail.com>
2025-08-22 01:55:20 -0600

    Remove unnecessary code

--- BasicShopAPI/Program.cs
+++ BasicShopAPI/Program.cs
@@ -1,9 +1,5 @@
 using BasicShopAPI;
-using BasicShopAPI.API.Mapping;
-using BasicShopAPI.Application.CQRS.Handlers.Products;
-using BasicShopAPI.Domain.Interfaces;
 using BasicShopAPI.Infrastructure.Persistence;
-using BasicShopAPI.Infrastructure.Repositories;
 using FluentMigrator.Runner;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,13 +15,11 @@ builder.Services
     .AddInfrastructure(builder.Configuration)
     .AddControllers();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("name=DefaultConnection", options => options.UseCompatibilityLevel(120)));
 
 var app = builder.Build();
 
+// Configure runner for the migrations
 using (var scope = app.Services.CreateScope())
 {
     var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
@@ -39,11 +33,10 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi("/openapi/v1.json");
 
-    // Swagger UI leyendo el documento nativo
+    // Swagger configuration
     app.UseSwaggerUI(opt =>
     {
-        opt.SwaggerEndpoint("/openapi/v1.json", "BasicShopAPI v1");
-        // opcional: servir la UI en /swagger (por defecto ya es /swagger)
+        opt.SwaggerEndpoint("/openapi/v1.json", "BasicShopAPI v1");        
         opt.RoutePrefix = "swagger";
     });
 }

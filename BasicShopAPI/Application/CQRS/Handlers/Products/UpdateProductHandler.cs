0437f2b7552b4102d517498b961d7553cc929431
Tito Calleros <tito.calleros@gmail.com>
2025-08-22 00:51:05 -0600

    Fix typo in UpdateProductCommand.Id

--- BasicShopAPI/Application/CQRS/Handlers/Products/UpdateProductHandler.cs
+++ BasicShopAPI/Application/CQRS/Handlers/Products/UpdateProductHandler.cs
@@ -9,7 +9,7 @@ namespace BasicShopAPI.Application.CQRS.Handlers.Products
 
         public async Task Handle(UpdateProductCommand cmd)
         {
-            var productoDb = await _repo.GetById(cmd.id) ?? throw new KeyNotFoundException("Product id not found");
+            var productoDb = await _repo.GetById(cmd.Id) ?? throw new KeyNotFoundException("Product id not found");
 
             productoDb.Update(cmd.Name, cmd.Description, cmd.Price, cmd.Stock);
             await _repo.Update(productoDb);

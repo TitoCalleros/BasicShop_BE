a6c1975ebc0381f5deb4fb10452bbfe8375f1f00
Tito Calleros <tito.calleros@gmail.com>
2025-08-22 18:55:17 -0600

    Implement handler to retrieve filtered product list

---
+++ BasicShopAPI/Application/CQRS/Handlers/Products/GetFilteredProductsHandler.cs
@@ -0,0 +1,33 @@
+﻿using BasicShopAPI.Application.CQRS.Queries.Products;
+using BasicShopAPI.Domain.Contracts;
+using BasicShopAPI.Domain.Entities;
+using BasicShopAPI.Domain.Interfaces;
+
+namespace BasicShopAPI.Application.CQRS.Handlers.Products
+{
+    public class GetFilteredProductsHandler(IProductRepository repo)
+    {
+        private readonly IProductRepository _repo = repo;
+
+        public async Task<QueryPagedResult<Product>> Handle(GetFilteredProductsQuery query)
+        {
+            var filter = new ProductFilter
+            {
+                Page = query.Page,
+                PageSize = query.PageSize,
+                Search = query.Search,
+                Sort = query.Sort,
+            };
+
+            var (items, total) = await _repo.GetPaged(filter);
+
+            return new QueryPagedResult<Product>
+            {
+                Items = items,
+                Page = filter.Page,
+                PageSize = filter.PageSize,
+                TotalCount = total
+            };
+        }
+    }
+}

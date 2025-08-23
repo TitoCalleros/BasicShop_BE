2d64574c27befd799850c5ec31ddd4c4bc2b70fe
Tito Calleros <tito.calleros@gmail.com>
2025-08-22 23:48:05 -0600

    Add ValidationAttribute to set de allowed values

---
+++ BasicShopAPI/API/Validations/AllowedValuesAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicShopAPI.API.Validations
+{
+    public class AllowedValuesAttribute: ValidationAttribute
+    {
+        private readonly string[] _allowed;
+
+        public AllowedValuesAttribute(params string[] allowed)
+        {
+            _allowed = allowed;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is null) return ValidationResult.Success;
+
+            if (value is string str && _allowed.Contains(str.ToLower()))
+                return ValidationResult.Success;
+
+            return new ValidationResult($"Gender must be one of: {string.Join(", ", _allowed)}");
+        }
+    }
+}

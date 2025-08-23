0b30479bf571942c90ccb5252f30285e848a34a0
Tito Calleros <tito.calleros@gmail.com>
2025-08-22 18:16:15 -0600

    Create class to filter product list

---
+++ BasicShopAPI/Domain/Contracts/ProductFilter.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicShopAPI.Domain.Contracts
+{
+    public sealed class ProductFilter
+    {
+        public int Page { get; init; } = 1;
+        public int PageSize { get; init; } = 20;
+
+        public string? Search { get; init; }
+
+        // Admite "name:asc", "name:desc", "price:asc", "price:desc", "stock:asc", "stock:desc"
+        // Default: "id:asc"
+        public string? Sort { get; init; }
+    }
+}

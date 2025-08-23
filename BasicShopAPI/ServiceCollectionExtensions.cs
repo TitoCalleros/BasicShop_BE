427dbda2b79183bc5dbbcadf8db35e8ba71f1c84
Tito Calleros <tito.calleros@gmail.com>
2025-08-22 23:04:45 -0600

    Remove unused code

--- BasicShopAPI/ServiceCollectionExtensions.cs
+++ BasicShopAPI/ServiceCollectionExtensions.cs
@@ -53,24 +53,11 @@ namespace BasicShopAPI
             var origins = config.GetSection("Cors:AllowedOrigins").Get<string[]>();
 
             services.AddCors(opt =>
-            {
-                // Política para desarrollo sin credenciales (simple)
+            {                
                 opt.AddPolicy("DevAll", p => p
                     .AllowAnyOrigin()
                     .AllowAnyHeader()
-                    .AllowAnyMethod());
-
-                //// Política con credenciales/orígenes explícitos
-                //opt.AddPolicy("WithCreds", p =>
-                //{
-                //    if (origins is { Length: > 0 })
-                //    {
-                //        p.WithOrigins(origins)
-                //         .AllowAnyHeader()
-                //         .AllowAnyMethod()
-                //         .AllowCredentials();
-                //    }
-                //});
+                    .AllowAnyMethod());                
             });
 
             return services;

2ea8dcca4455ce0d571cf4afad544c9bfa8e600e
Tito Calleros <tito.calleros@gmail.com>
2025-08-24 14:26:16 -0600

    Correct max length for Description to 250

--- BasicShopAPI/API/DTOs/Products/CreateProductRequestDTO.cs
+++ BasicShopAPI/API/DTOs/Products/CreateProductRequestDTO.cs
@@ -7,7 +7,7 @@ namespace BasicShopAPI.API.DTOs.Products
         [Required, StringLength(50)]
         public required string Name { get; set; }
 
-        [StringLength(150)]
+        [StringLength(250)]
         public string? Description { get; set; }
         [Required, Range(0.01, double.MaxValue)]
         public decimal Price { get; set; }
